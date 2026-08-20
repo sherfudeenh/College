dcc818b36cafdc6392e17deafd7ee8c537cdc023
sherfudeenh <sherfudeen22@gmail.com>
2026-08-20 07:39:43 +0530

    Create Procstr.java

---
+++ Semester_5/Computer_Networks/EX1/Leetcode/Procstr.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Procstr {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        while (T-- > 0) {
+            String s = sc.next();
+            int sum = 0;
+
+            for (int i = 0; i < s.length(); i++) {
+                char ch = s.charAt(i);
+
+                if (Character.isDigit(ch)) {
+                    sum += ch - '0';
+                }
+            }
+
+            System.out.println(sum);
+        }
+
+        sc.close();
+    }
+}
+
